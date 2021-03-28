196b1a4d27b2aec09e86eddf3ade1c89e3408f79
cjshinningg <784319417@qq.com>
2021-03-28 21:17:35 +0800

    Bundler 源码编写2

--- bundler.js
+++ bundler.js
@@ -32,4 +32,11 @@ const moduleAnalyser = (filename) => {
   }
 }
 
-moduleAnalyser('./src/index.js')
+moduleAnalyser('./src/index.js');
+
+// 大致步骤：
+// 1、分析入口文件，函数moduleAnalyser
+// 2、拿到入口文件名，先去读取文件内容，然后借助@babel/parser来把读取回来的字符串转化成抽象语法树。
+// 3、分析抽象语法树中声明所在位置，通过ImportDeclaration找到import语句对应的内容，找到后分析里面的依赖内容并拼装为一个js对象放到dependentcies，这个变量存储所有的依赖关系
+// 4、将ast树转换成浏览器可以执行的代码
+// 5、最终返回入口名字，依赖关系，打包源码
